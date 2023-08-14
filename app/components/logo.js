7b7178077ee5485830cacde25a614da683e84345
Shenghao Xie <xshxiegudong45@gmail.com>
2023-08-15 00:20:30 +0800

    finish navbar

--- app/components/logo.js
+++ app/components/logo.js
@@ -1,40 +1,32 @@
-import Link from 'next/link';
-import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const LogoBox = styled.span`
-    font-weight:bold;
-    font-size: 18px;
-    display: inline-flex;
-    align-items: center;
-    height:30px;
-    line-height: 20px;
-    padding: 10px;
-    &:hover img{
-        transform: rotate(20deg);
-}
-`
+  font-weight: bold;
+  font-size: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
+  &:hover img {
+    transform: rotate(20deg);
+  }
+`;
 
 const Logo = () => {
-    const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
-    return (
-        <Link href="/">
-            <a>
-                <LogoBox>
-                    <Image src={footPrintImg} width={20} height={20} alt="logo" />
-                    <Text
-                        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                        fontFamily="M PLUS Rounded 1c"
-                        fontWeight="bold"
-                        ml={3}
-                    >
-                        Shenghao Xie
-                    </Text>
-                </LogoBox>
-            </a>
-        </Link>
-    )
-}
+  const footPrintImg = `avatar.svg`;
+  return (
+    <Link href="/">
+      <LogoBox>
+        <Image src={footPrintImg} width={35} height={35} alt="avatar" />
+      </LogoBox>
+    </Link>
+  );
+};
 
 export default Logo;
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,28 @@
-"use client"
-import { Box, ChakraProvider, Container, Heading } from '@chakra-ui/react'
-
+'use client';
+import Navbar from '@/app/components/navbar';
+import { Box, ChakraProvider, Container, Heading } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-    return (
-
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <ChakraProvider>
-                <Container>
-                    <Box borderRadius="lg" bg="red" p={3} mb={6}>
-                        Hello, I&apos;m a full-stack developer based in China!
-                    </Box>
-                    <Box display={{ md: "flex" }}>
-                        <Box flexGrow={1}>
-                            <Heading as="h2" variant="page-title">
-                                Shenghao Xie
-                            </Heading>
-                            <p>xiegudong45(Developer)</p>
-                        </Box>
-                    </Box>
-                </Container>
-            </ChakraProvider>
-        </main>
-    )
+  const router = useRouter();
+  return (
+    <>
+      <Navbar path={router} />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Container>
+          <Box borderRadius="lg" bg="red" p={3} mb={6}>
+            Hello, I&apos;m a full-stack developer based in China!
+          </Box>
+          <Box display={{ md: 'flex' }}>
+            <Box flexGrow={1}>
+              <Heading as="h2" variant="page-title">
+                Shenghao Xie
+              </Heading>
+              <p>xiegudong45(Developer)</p>
+            </Box>
+          </Box>
+        </Container>
+      </main>
+    </>
+  );
 }
